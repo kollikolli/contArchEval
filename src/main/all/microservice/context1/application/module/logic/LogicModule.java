e023e378445780fd0503c9d106e8e420ed818cb2
kollegger <manuel.kollegger@gmail.com>
2017-11-28 21:50:23 +0100

    Addition of an Annotation

--- src/main/all/microservice/context1/application/module/logic/LogicModule.java
+++ src/main/all/microservice/context1/application/module/logic/LogicModule.java
@@ -1,6 +1,6 @@
 package microservice.context1.application.module.logic;
 
-
+@MyAnnotation(myValue = 1)
 public class LogicModule {
 
 
---
+++ src/main/all/microservice/context1/application/module/logic/MyAnnotation.java
@@ -0,0 +1,5 @@
+package microservice.context1.application.module.logic;
+
+public @interface MyAnnotation {
+    int myValue();
+}
