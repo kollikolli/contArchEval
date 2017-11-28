c619768fc609161b39892088bc1b3d89adadd167
kollegger <manuel.kollegger@gmail.com>
2017-11-28 21:46:49 +0100

    Architecture violation: layering - logic layer accesses api layer

--- src/main/all/microservice/context1/application/module/logic/LogicModule.java
+++ src/main/all/microservice/context1/application/module/logic/LogicModule.java
@@ -1,8 +1,10 @@
 package microservice.context1.application.module.logic;
 
 
+import microservice.context1.application.module.api.ApiModule;
+
 public class LogicModule {
 
-
+    ApiModule a = new ApiModule();
 }
 
