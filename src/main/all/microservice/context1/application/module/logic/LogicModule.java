90e97f75991b734af5fdf989cacb57805850bb05
kollegger <manuel.kollegger@gmail.com>
2017-11-28 21:48:18 +0100

    Revert "Architecture violation: layering - logic layer accesses api layer"
    This reverts commit c619768

--- src/main/all/microservice/context1/application/module/logic/LogicModule.java
+++ src/main/all/microservice/context1/application/module/logic/LogicModule.java
@@ -1,10 +1,8 @@
 package microservice.context1.application.module.logic;
 
 
-import microservice.context1.application.module.api.ApiModule;
-
 public class LogicModule {
 
-    ApiModule a = new ApiModule();
+
 }
 
